493bcd5d9bb5eb2c218db0db19510cff9866f68e
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-05-05 21:43:16 +0600

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,63 +1,50 @@
 import React, { useContext, useState } from 'react';
 import ActiveLink from './ActiveLink/ActiveLink';
 import { AuthContext } from '../authProvider/AuthProvider';
+import { FaBars } from "react-icons/fa";
+
 
 
 const Header = () => {
+
     const { user, logOut } = useContext(AuthContext);
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const [toggler, setToggler] = useState(false);
+
+    // console.log(user?.photoURL);
 
     return (
         <div className='grid grid-cols-2 py-3'>
             <div className='flex items-center'>
                 <div className='h-10'>
-                    <img className='h-full rounded-md' src='https://i.ibb.co/k8gmPns/logo.png' alt='' />
+                    <img className='h-full rounded-md' src="https://i.ibb.co/k8gmPns/logo.png" alt="" />
                 </div>
             </div>
 
-            <div className='flex justify-end items-center text-center py-2'>
-                <button className='mr-4 font-bold md:hidden' onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                    {/* Menu bar icon */}
-                    {isMenuOpen ? (
-                        <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M6 18L18 6M6 6l12 12' />
-                        </svg>
-                    ) : (
-                        <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16M4 18h16' />
-                        </svg>
-                    )}
-                </button>
 
-                <div className={`md:flex ${isMenuOpen ? 'block' : 'hidden'}`}>
-                    <ActiveLink className='block mt-4 md:inline-block md:mt-0 md:mr-4 font-bold' to='/'>
-                        <button>Home</button>
-                    </ActiveLink>
-                    <ActiveLink className='block mt-4 md:inline-block md:mt-0 md:mr-4 font-bold' to='/blog'>
-                        <button>Blog</button>
-                    </ActiveLink>
+            <div className={`${toggler ? 'top-14 right-8' : '-top-64 right-8' } md:flex justify-end items-center absolute md:static duration-500 p-2 rounded-md md:bg-white bg-pink-200`}>
+                <ActiveLink className='mr-4 font-bold' to='/'><button>Home</button></ActiveLink>
+                <ActiveLink className='mr-4 font-bold' to='/blog'><button>Blog</button></ActiveLink>
 
-                    {user ? (
+                {
+                    user ?
                         <>
-                            <button onClick={logOut} className='text-yellow-400 font-bold py-2 px-3 bg-rose-500 rounded-md'>
-                                Sign out
-                            </button>
-                            <span className='w-10 ml-4'>
-                                <img className='w-full rounded-full' src={user?.photoURL} alt='' />
-                            </span>
-                        </>
-                    ) : (
+                            <button onClick={logOut} className='text-yellow-400 font-bold py-2 px-3 bg-rose-500 rounded-md'>Sign out</button>
+                            <span className='md:w-10 w-4 ml-4'><img className='w-full rounded-full' src={user?.photoURL} alt="" /></span>
+                        </> :
                         <>
-                            <ActiveLink className='block mt-4 md:inline-block md:mt-0 md:mr-4 font-bold' to='/register'>
-                                <button>Register</button>
-                            </ActiveLink>
-                            <ActiveLink className='block md:inline-block font-bold' to='/login'>
-                                Sign in
-                            </ActiveLink>
+                            <ActiveLink className='mr-4 font-bold' to='/register'><button>Register</button></ActiveLink>
+                            <ActiveLink className='font-bold' to='/login'>Sign in</ActiveLink>
                         </>
-                    )}
-                </div>
+                } 
+
             </div>
+
+            <div className='md:hidden flex justify-end items-center' onClick={() => setToggler(!toggler)}>
+                    <FaBars />
+            </div>
+
+
         </div>
     );
 };
--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -5,17 +5,17 @@ const HomeBanner = () => {
         <div className='mb-10' style={{ backgroundImage: `url("https://i.ibb.co/TTpnhbj/food.png")`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', height: '100vh', borderRadius:'10px'}}>
             <div className='w-full lg:w-1/2 md:w-3/4 h-full flex flex-col justify-center p-4'>
                 <p className='text-pink-500'><small>#cooking recipe</small></p>
-                <h1 className='text-6xl text-pink-400 font-bold my-4'>Simple way to find yammy recipes</h1>
-                <p className='text-stone-300 font-semibold my-3'>Food is one of the basic necessities of life. Food contains nutrients—substances essential
+                <h1 className='text-2xl md:text-4xl lg:text-6xl text-pink-400 font-bold my-4'>Simple way to find yammy recipes</h1>
+                <p className='text-stone-300 font-normal lg:font-semibold my-3'>Food is one of the basic necessities of life. Food contains nutrients—substances essential
                     for the growth, repair, and maintenance of body tissues and for the regulation of vital processes. Nutrients provide the energy our bodies
-                    need to function. The energy in food is measured in units called calories.
+                    need to function.
                 </p>
                 <div className='flex'>
                     <div className='mr-16'>
-                        <h1 className='text-4xl text-rose-600'>320K <sup className='text-sm font-medium text-rose-400'>Active Users</sup></h1>
+                        <h1 className='lg:text-4xl md:text-3xl text-xl text-rose-600'>320K <sup className='text-sm font-medium text-rose-400'>Active Users</sup></h1>
                     </div>
                     <div>
-                        <h1 className='text-4xl text-rose-600'>4.2 <sup className='text-sm font-medium text-rose-400'>Avg. Rating</sup></h1>
+                        <h1 className='lg:text-4xl md:text-3xl text-xl text-rose-600'>4.2 <sup className='text-sm font-medium text-rose-400'>Avg. Rating</sup></h1>
                     </div>
                 </div>
 
