4cd892d32a6b80bc7721d9f3edeba1d9818ef8aa
pijushwebdev <pijushsarker.dev@gmail.com>
2025-01-01 18:32:06 +0600

    fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,106 +1,132 @@
-import React, { useContext, useRef, useState } from 'react';
-import { AuthContext } from '../authProvider/AuthProvider';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-import { sendPasswordResetEmail} from "firebase/auth";
-import { toast } from 'react-toastify';
+import React, { useContext, useRef, useState } from "react";
+import { AuthContext } from "../authProvider/AuthProvider";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
 
 const Login = () => {
-    const [user, setUser] = useState(null);
-    const emailRef = useRef();
+  const [user, setUser] = useState(null);
+  const emailRef = useRef();
 
-    const location = useLocation();
+  const location = useLocation();
 
+  const from = location.state?.from?.pathname || "/";
 
-    const from = location.state?.from?.pathname || '/';
+  const { signIn, signInWithGoogle, signInWithGitHub, passwordReset } =
+    useContext(AuthContext);
+  const navigate = useNavigate();
 
+  const handleGoogleSignIn = () => {
+    signInWithGoogle()
+      .then((result) => {
+        const signedUser = result.user;
+        setUser(signedUser);
+        navigate(from, { replace: true });
+        toast.success("Successfully Login");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
 
-    const { signIn, signInWithGoogle, signInWithGitHub, passwordReset } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const handleGitHubSignIn = () => {
+    signInWithGitHub()
+      .then((result) => {
+        const signedUser = result.user;
+        setUser(signedUser);
+        navigate(from, { replace: true });
+        toast.success("Successfully Login");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
 
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
 
-    const handleGoogleSignIn = () => {
-        signInWithGoogle()
-            .then(result => {
-                const signedUser = result.user;
-                setUser(signedUser);
-                navigate(from, {replace: true});
-                toast.success('Successfully Login')
-            })
-            .catch(error => {
-                toast.error(error.message)
-            })
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then((res) => {
+        const userEP = res.user;
+
+        setUser(userEP);
+        toast.success("Successfully Login");
+        // console.log(userEP);
+        navigate(from, { replace: true });
+      })
+      .catch((error) => toast.error(error.message));
+  };
+
+  const handleResetPassword = (event) => {
+    const email = emailRef.current.value;
+    if (!email) {
+      toast.error("please provide email");
     }
+    passwordReset(email).then(() => {
+      toast.error("a reset mail has been sent to your mail");
+    });
+  };
 
-    const handleGitHubSignIn = () => {
-        signInWithGitHub()
-            .then(result => {
-                const signedUser = result.user;
-                setUser(signedUser);
-                navigate(from, {replace: true});
-                toast.success('Successfully Login')
-                
-            })
-            .catch(error => {
-                toast.error(error.message)
+  return (
+    <div className="my-8 flex justify-center">
+      <div className="shadow-md w-full lg:w-1/2 rounded-md p-5">
+        <form onSubmit={handleSignIn} className="flex flex-col items-center">
+          <input
+            className="py-1 px-3 rounded-md bg-[#faf5f9] border focus:border-[#D287C5] text-black outline-none"
+            ref={emailRef}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter email"
+            required
+          />
+          <input
+            className="my-5 py-1 px-3 rounded-md bg-[#faf5f9] border focus:border-[#D287C5] text-black outline-none"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter password"
+            required
+          />
+          <input
+            className="py-1 px-3 bg-[#8b207a] w- rounded-md text-white"
+            type="submit"
+            value="Login"
+          />
+          <p>
+            <small>
+              Forget Password?{" "}
+              <button onClick={handleResetPassword}>Reset</button>
+            </small>
+          </p>
+          <p>
+            <small>
+              No account? <Link to="/register">SignUp</Link>
+            </small>
+          </p>
 
-            })
-    }
-
-    const handleSignIn = (e) => {
-        e.preventDefault();
-        const form = e.target;
-
-        const email = form.email.value;
-        const password = form.password.value;
-
-        signIn(email, password)
-            .then(res => {
-                
-                const userEP = res.user;
-                
-                setUser(userEP);
-                toast.success('Successfully Login')
-                // console.log(userEP);
-                navigate(from, {replace: true});
-
-            })
-            .catch(error => toast.error(error.message))
-    }
-
-    const handleResetPassword = event => {
-        const email = emailRef.current.value;
-        if (!email) {
-            toast.error('please provide email');
-        }
-        passwordReset(email)
-            .then(() => {
-                toast.error('a reset mail has been sent to your mail');
-            })
-    }
-
-
-    return (
-        <div className='my-8 flex justify-center'>
-            
-                <div className='shadow-md w-full lg:w-1/2 rounded-md p-5'>
-                    <form onSubmit={handleSignIn} className='flex flex-col items-center'>
-
-                        <input className='py-1 px-3 rounded-md bg-cyan-800 text-white outline-none' ref={emailRef} type="email" name='email' id='email' placeholder='Enter email' required />
-                        <input className='my-5 py-1 px-3 rounded-md bg-cyan-800 text-white outline-none' type="password" name='password' id='password' placeholder='Enter password' required />
-                        <input className='py-1 px-2 bg-green-700 rounded-md text-white' type="submit" value='SignIn' />
-                        <p><small>Forget Password? <button onClick={handleResetPassword}>Reset</button></small></p>
-                        <p><small>No account? <Link to='/register'>SignUp</Link></small></p>
-
-                        <div className='mt-5'>
-                            <button className='rounded-md py-1 px-2 bg-fuchsia-500 text-zinc-50 mr-5' onClick={handleGoogleSignIn}>Sign in with Google</button>
-                            <button className='rounded-md py-1 px-2 bg-emerald-600 text-zinc-50' onClick={handleGitHubSignIn}>Sign in with gitHub</button>
-                        </div>
-
-                    </form>
-
-                </div>
-        </div>
-    );
+          <div className="mt-5">
+            <button
+              className="rounded-md py-1 px-2 bg-[#8b207a] text-zinc-50 mr-5"
+              onClick={handleGoogleSignIn}
+            >
+              Sign in with Google
+            </button>
+            <button
+              className="rounded-md py-1 px-2 bg-[#b841a4] text-zinc-50"
+              onClick={handleGitHubSignIn}
+            >
+              Sign in with gitHub
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,83 +1,109 @@
-import React, { useContext } from 'react';
-import { sendEmailVerification, updateProfile } from 'firebase/auth';
-import { Link, useNavigate} from 'react-router-dom';
-import { AuthContext } from '../authProvider/AuthProvider';
-import { validPassword, validEmail } from './RegEx';
-import { toast } from 'react-toastify';
-
+import React, { useContext } from "react";
+import { sendEmailVerification, updateProfile } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../authProvider/AuthProvider";
+import { validPassword, validEmail } from "./RegEx";
+import { toast } from "react-toastify";
 
 const Register = () => {
+  const { user, createUser, logOut } = useContext(AuthContext);
 
-    const { user, createUser , logOut} = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  // const verifyUserEmail = (email) => {
+  //     sendEmailVerification(email);
+  //     toast.success('A verification mail has been sent to your mail');
 
+  // }
+  const updateUserProfile = (user, name, photoUrl) => {
+    updateProfile(user, {
+      displayName: name,
+      photoURL: photoUrl,
+    })
+      .then(() => {})
+      .catch((err) => console.log(err.message));
+  };
 
-    
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // console.log(e.target);
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const name = form.name.value;
+    const photoURL = form.photo.value;
 
-    // const verifyUserEmail = (email) => {
-    //     sendEmailVerification(email);
-    //     toast.success('A verification mail has been sent to your mail');
-
-    // }
-    const updateUserProfile = (user, name,photoUrl) => {
-        updateProfile(user, {
-            displayName: name,
-            photoURL: photoUrl
-        })
-            .then(() => { })
-            .catch(err => console.log(err.message))
+    if (!validEmail.test(email)) {
+      toast.error("Email not valid");
+      return;
+    }
+    if (!validPassword.test(password)) {
+      toast.error("Password not valid");
+      return;
     }
 
-    const handleRegister = (e) => {
-        e.preventDefault();
-        // console.log(e.target);
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        const name = form.name.value;
-        const photoURL = form.photo.value;
-
-
-        if (!validEmail.test(email)) {
-            toast.error("Email not valid")
-            return;
-        } 
-        if (!validPassword.test(password)) {
-            toast.error("Password not valid")
-            return;
-        }
-
-        createUser(email, password)
-            .then(res => {
-                const loggedUser = res.user;
-                // console.log(loggedUser);
-                form.reset();
-                // verifyUserEmail(loggedUser);
-                updateUserProfile(loggedUser, name, photoURL)
-                toast.success("Successfully created user");
-                logOut();
-                navigate('/login');
-                
-            })
-            .catch(error => toast.error(error.message))
-
-    }
-    return (
-        <div className='my-6 flex justify-center'>
-            <div className='shadow-md my-10 p-5 lg:w-1/2 w-full'>
-                <form onSubmit={handleRegister} className='flex flex-col items-center'>
-                    <input className='py-1 mb-5 px-3 rounded-md bg-cyan-800 text-white outline-none' type="text" name='name' id='name' placeholder='Enter name' required />
-                    <input className='py-1 mb-5 px-3 rounded-md bg-cyan-800 text-white outline-none' name='photo' id='photo' placeholder='photo url (optional)' type="text" />
-                    <input className='py-1 px-3 rounded-md bg-cyan-800 text-white outline-none' type="email" name='email' id='email' placeholder='Enter email' required />
-                    <input className='my-5 py-1 px-3 rounded-md bg-cyan-800 text-white outline-none' type="password" name='password' id='password' placeholder='Enter password' required />
-                    <input className='py-1 px-2 bg-green-700 rounded-md text-white' type="submit" value='SignUp' />
-                    <p><small>Already have an account? <Link to='/login'>SignIn</Link></small></p>
-                </form>
-            </div>
-
-        </div>
-    );
+    createUser(email, password)
+      .then((res) => {
+        const loggedUser = res.user;
+        // console.log(loggedUser);
+        form.reset();
+        // verifyUserEmail(loggedUser);
+        updateUserProfile(loggedUser, name, photoURL);
+        toast.success("Successfully created user");
+        logOut();
+        navigate("/login");
+      })
+      .catch((error) => toast.error(error.message));
+  };
+  return (
+    <div className="my-6 flex justify-center">
+      <div className="shadow-md my-10 p-5 lg:w-1/2 w-full">
+        <form onSubmit={handleRegister} className="flex flex-col items-center">
+          <input
+            className="py-1 mb-5 px-3 rounded-md bg-[#faf5f9] border focus:border-[#D287C5] text-black outline-none"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Enter name"
+            required
+          />
+          <input
+            className="py-1 mb-5 px-3 rounded-md bg-[#faf5f9] border focus:border-[#D287C5] text-black outline-none"
+            name="photo"
+            id="photo"
+            placeholder="photo url (optional)"
+            type="text"
+          />
+          <input
+            className="py-1 px-3 rounded-md bg-[#faf5f9] border focus:border-[#D287C5] text-black outline-none"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter email"
+            required
+          />
+          <input
+            className="my-5 py-1 px-3 rounded-md bg-[#faf5f9] border focus:border-[#D287C5] text-black outline-none"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter password"
+            required
+          />
+          <input
+            className="py-1 px-2 bg-[#8b207a] rounded-md text-white"
+            type="submit"
+            value="SignUp"
+          />
+          <p>
+            <small>
+              Already have an account? <Link to="/login">SignIn</Link>
+            </small>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
